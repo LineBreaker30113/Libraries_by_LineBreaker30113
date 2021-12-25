f56abfce4cf242412496817b2bc807b8b141966a
LineBreaker30113 <69761063+LineBreaker30113@users.noreply.github.com>
2021-12-25 17:06:37 +0300

    Update tbridge.hpp

--- threadUtils/tbridge.hpp
+++ threadUtils/tbridge.hpp
@@ -2,10 +2,11 @@
 #include <mutex>
 
 namespace aetl {// Abdulhalim ESEN's Template Library
-	template<typename contentT, typename refCntrT = std::uint8_t>// referenceCounterType
-	struct tbridge {// actually just a mutext and dynamic memory manupilation wrapper for a type called contentT
+	// actually just a mutext and dynamic memory manupilation wrapper for a type called contentT
+	template<typename contentT, typename refCntrT = std::uint8_t> struct tbridge {
+
 		struct zzm_bridgeT { contentT content; refCntrT refCntr = 1; std::mutex mutex; };
-		zzm_bridgeT* zzm; contentT& onstart(); constexpr void on_end();
+		zzm_bridgeT* zzm; contentT& onstart(); constexpr void on_end(); const contentT& readnow();
 
 		operator zzm_bridgeT& (); tbridge(); tbridge(zzm_bridgeT other); ~tbridge();
 
@@ -14,10 +15,12 @@ namespace aetl {// Abdulhalim ESEN's Template Library
 	template<typename cT, typename rCT> inline cT& tbridge<cT, rCT>::onstart() { zzm->mutex.lock(); return zzm->content; }
 	template<typename cT, typename rCT> inline constexpr void tbridge<cT, rCT>::on_end() { zzm->mutex.unlock(); }
 
+	template<typename cT, typename rCT> inline const cT& tbridge<cT, rCT>::readnow() { return zzm->content; }
+
 	template<typename cT, typename rCT> inline tbridge<cT, rCT>::operator zzm_bridgeT& () { return zzm->content; }
 	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge() : zzm(new zzm_bridgeT) {}
 	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge(zzm_bridgeT other)
-		:zzm(std::move(other)) { zzm->refCntr++; }
+		: zzm(std::move(other)) { zzm->refCntr++; }
 	template<typename contentT, typename refCntrT> inline tbridge<contentT, refCntrT>::~tbridge()
 	{ zzm->mutex.lock(); zzm->refCntr--; if (!zzm->refCntr) { zzm->mutex.unlock(); delete zzm; return; } zzm->mutex.unlock(); }
 
