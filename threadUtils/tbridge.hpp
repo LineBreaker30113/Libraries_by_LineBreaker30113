f7360482ab5c9f1750c355c5b422e3b6647d3225
LineBreaker30113 <69761063+LineBreaker30113@users.noreply.github.com>
2022-01-07 23:30:27 +0300

    Update tbridge.hpp

--- threadUtils/tbridge.hpp
+++ threadUtils/tbridge.hpp
@@ -2,33 +2,6 @@
 #include <mutex>
 
 namespace aetl {// Abdulhalim ESEN's Template Library
-	// actually just a mutext and dynamic memory manupilation wrapper for a type called contentT
-	template<typename contentT, typename refCntrT = std::uint8_t> struct tbridge {
-
-		struct zzm_bridgeT { contentT content; refCntrT refCntr = 1; std::mutex mutex; };
-		zzm_bridgeT* zzm; contentT& onstart(); constexpr void on_end(); const contentT& readnow();
-
-		operator zzm_bridgeT*& (); tbridge(); tbridge(zzm_bridgeT* other); ~tbridge(); constexpr void zzzm_onDelete();
-
-		constexpr void operator=(zzm_bridgeT* other);
-	};
-
-	template<typename cT, typename rCT> inline cT& tbridge<cT, rCT>::onstart() { zzm->mutex.lock(); return zzm->content; }
-	template<typename cT, typename rCT> inline constexpr void tbridge<cT, rCT>::on_end() { zzm->mutex.unlock(); }
-
-	template<typename cT, typename rCT> inline const cT& tbridge<cT, rCT>::readnow() { return zzm->content; }
-
-	template<typename cT, typename rCT> inline tbridge<cT, rCT>::operator zzm_bridgeT*& () { return zzm->content; }
-	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge() : zzm(new zzm_bridgeT) {}
-	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge(zzm_bridgeT* other)
-		: zzm(std::move(other)) { zzm->refCntr++; }
-	template<typename cT, typename rCT> inline tbridge<cT, rCT>::~tbridge()	{ zzzm_onDelete(); }
-
-	template<typename cT, typename rCT>	inline constexpr void tbridge<cT, rCT>::zzzm_onDelete()
-	{ zzm->mutex.lock(); zzm->refCntr--; if (!zzm->refCntr) { zzm->mutex.unlock(); delete zzm; return; } zzm->mutex.unlock(); }
-
-	template<typename cT, typename rCT>	inline constexpr void tbridge<cT, rCT>::operator=(zzm_bridgeT* other)
-	{ zzzm_onDelete(); other->mutex.lock(); zzm = std::move(other); zzm->refCntr++; zzm->mutex.unlock; }
 
 	/*example use:
 	* ...
@@ -51,4 +24,41 @@ namespace aetl {// Abdulhalim ESEN's Template Library
 	* ...
 	* }
 	*/
+
+	// actually just a mutext and dynamic memory manupilation wrapper for a type called contentT
+	template<typename contentT = int, typename refCntrT = std::uint8_t> struct tbridge {
+
+		struct zzm_bridgeT { contentT content; refCntrT refCntr = 1; std::mutex mutex; };
+		zzm_bridgeT* zzm; contentT& onstart(); constexpr void on_end(); const contentT& readnow();
+
+		tbridge(const tbridge& other); tbridge(tbridge&& other);
+		tbridge(); ~tbridge(); tbridge(const contentT& val); tbridge(contentT&& val);
+		constexpr void operator=(const tbridge& other); constexpr void operator=(tbridge&& other);
+		constexpr void zzzm_onDelete(); constexpr void zzzm_doCopy(const tbridge& other);
+	};
+
+	template<typename cT, typename rCT> inline cT& tbridge<cT, rCT>::onstart() { zzm->mutex.lock(); return zzm->content; }
+	template<typename cT, typename rCT> inline constexpr void tbridge<cT, rCT>::on_end() { zzm->mutex.unlock(); }
+	template<typename cT, typename rCT> inline const cT& tbridge<cT, rCT>::readnow() { return zzm->content; }
+
+	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge() : zzm(new zzm_bridgeT) {}
+	template<typename cT, typename rCT> inline tbridge<cT, rCT>::~tbridge() { zzzm_onDelete(); }
+	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge(tbridge&& other) : zzm(std::move(other.zzm)) {}
+	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge(const tbridge& other) { zzzm_doCopy(other); }
+
+	template<typename cT, typename rCT>	inline tbridge<cT, rCT>::tbridge(const cT& val) : zzm(new zzm_bridgeT)
+	{ zzm->mutex.lock(); zzm->content = val; zzm->mutex.unlock(); }
+	template<typename cT, typename rCT> inline tbridge<cT, rCT>::tbridge(cT&& val) : zzm(new zzm_bridgeT)
+	{ zzm->mutex.lock(); zzm->content = std::move(val); zzm->mutex.unlock(); }
+
+	template<typename cT, typename rCT>	inline constexpr void tbridge<cT, rCT>::operator=(const tbridge& other)
+	{ zzzm_onDelete(); zzzm_doCopy(other); }
+	template<typename cT, typename rCT>	inline constexpr void tbridge<cT, rCT>::operator=(tbridge&& other)
+	{ zzzm_onDelete(); }
+
+
+	template<typename cT, typename rCT>	inline constexpr void tbridge<cT, rCT>::zzzm_onDelete()
+	{ zzm->mutex.lock(); zzm->refCntr--; if (!zzm->refCntr) { zzm->mutex.unlock(); delete zzm; return; } zzm->mutex.unlock(); }
+	template<typename cT, typename rCT>	inline constexpr void tbridge<cT, rCT>::zzzm_doCopy(const tbridge& other)
+	{ other.zzm->mutex.lock(); zzm = other.zzm; zzm->refCntr++; zzm->mutex.unlock(); }
 }
