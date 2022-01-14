c4ddebee476ef9963528862d41afb09ac0ab08ff
LineBreaker30113 <69761063+LineBreaker30113@users.noreply.github.com>
2022-01-14 19:53:00 +0300

    Update referenceT.hpp

--- cpp/basicUtils/referenceT.hpp
+++ cpp/basicUtils/referenceT.hpp
@@ -2,10 +2,10 @@
 
 namespace ael {// Abdulhalim ESEN's Template Library
 
-	template<typename T> struct reference {// a pointer acts as an reference
+	template<typename T> struct referenceT {// a pointer acts as an referenceT
 		T* p;// the variable pointer
 		operator T& () const { return *p; }
-		reference() : p(nullptr) {} reference(T& var) : p(&var) {} T& operator() (T& var) { p = &var; return *p; }
+		referenceT() : p(nullptr) {} referenceT(T& var) : p(&var) {} T& operator() (T& var) { p = &var; return *p; }
 
 		template<typename T0> const T& operator+ (const T0& r) const { return (*p) + r; }
 		template<typename T0> const T& operator- (const T0& r) const { return (*p) - r; }
@@ -49,6 +49,6 @@ namespace ael {// Abdulhalim ESEN's Template Library
 		template<typename T0> T& operator-> () const { return *p; }
 
 	};
-	template<typename T> const T& operator++ (reference<T> ref) { return ((*ref.p)++); }
-	template<typename T> const T& operator-- (reference<T> ref) { return ((*ref.p)--); }
+	template<typename T> const T& operator++ (const referenceT<T>& ref) { return ((*ref.p)++); }
+	template<typename T> const T& operator-- (const referenceT<T>& ref) { return ((*ref.p)--); }
 }
